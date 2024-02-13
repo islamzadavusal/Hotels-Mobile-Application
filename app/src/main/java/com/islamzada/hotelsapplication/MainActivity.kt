2f780065946f4df480a749fe905f6c20a764b3f7
Vusal Islmazada <vusal.islamzada.01@gmail.com>
2024-02-13 09:26:53 +0400

    Solve Fix

--- app/src/main/java/com/islamzada/hotelsapplication/MainActivity.kt
+++ app/src/main/java/com/islamzada/hotelsapplication/MainActivity.kt
@@ -11,15 +11,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var auth: FirebaseAuth
-
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigationApp) as NavHostFragment
         NavigationUI.setupWithNavController(binding.bottomNavBar, navHostFragment.navController)
@@ -38,6 +35,12 @@ class MainActivity : AppCompatActivity() {
     }
     }
 }
+
+
+
+
+//    private lateinit var auth: FirebaseAuth
+
 
 //        auth = FirebaseAuth.getInstance()
 //
